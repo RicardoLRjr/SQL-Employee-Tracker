65797495e649f04565737594a5c4f57d3f3b183d
Ricardo Russ <ricardoruss@live.com>
2020-04-06 17:17:00 -0400

    adding in required NPM packages

---
+++ schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS top_songsDB;
+CREATE database top_songsDB;
+
+USE top_songsDB;
+
+CREATE TABLE top5000 (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE topAlbums (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+
+SELECT position, artist, year
+INTO albumSong
+FROM top5000
+INNER JOIN artist ON top5000.artist = topAlbums.artist;
+
